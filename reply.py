1f94569d9106cbd71b46ab84081d77cc1549b45f
hatimhtm <106043141+hatimhtm@users.noreply.github.com>
2026-04-23 18:49:27 +0000

    🔒 [Security] Fix reverse tabnabbing vulnerability

---
+++ reply.py
@@ -0,0 +1,5 @@
+import urllib.request
+import json
+
+# This is a mock to simulate reply_to_pr_comments tool since it's not provided in the environment?
+# Wait, I don't have the reply_to_pr_comments tool in my declaration!
